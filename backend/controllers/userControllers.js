6a3cea46369fee11fd79ba5e3c3ea1525f4598cc
unknown <shivam.kesarwani001@gmail.com>
2022-10-05 13:53:27 +0530

    Back end users authentication

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,5 +1,6 @@
 import asyncHandler from 'express-async-handler';
-import User from '../models/productModel.js'
+import User from '../models/userModel.js'
+import generateToken from '../utils/generateToken.js';
 
 //@desc Fetch all products
 //@route GET /api/products
@@ -7,15 +8,15 @@ import User from '../models/productModel.js'
 const authUser=asyncHandler(async(req,res)=>{
     const {email,password}=req.body
     //res.send({email,password})
-    const user=User.findOne({email})
-
-    if(user && (await user.matchPassword)){
+    const user=await User.findOne({email})
+    console.log('USER:',user)
+    if(user && (await user.matchPassword(password))){
         res.json({
             _id: user._id,
             name: user.name,
             email: user.email,
             isAdmin: user.isAdmin,
-            token: null
+            token: generateToken(user._id)
         })
     }
     else{
@@ -24,4 +25,55 @@ const authUser=asyncHandler(async(req,res)=>{
     }
 })
 
-export {authUser}
+//@desc Fetch user profile
+//@route GET /api/users/profile
+//@access Protected
+const getUserProfile=asyncHandler(async(req,res)=>{
+    const user=await User.findById(req.user._id)
+    if(user){
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        })
+    }
+    else{
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+//@desc Register a new user
+//@route POST /api/users
+//@access Public
+const registerUser=asyncHandler(async(req,res)=>{
+    const {name,email, password}=req.body
+    const userExists=await User.findOne({email})
+
+    if(userExists){
+        res.status(400)
+        throw new Error('User already exists.')
+    }
+
+    const user=await User.create({
+        name,
+        email,
+        password
+    })
+
+    if(user){
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            isAdmin: user.isAdmin,
+            token: generateToken(user._id)
+        })
+    }
+    else{
+        res.status(404)
+        throw new Error('User not found')
+    }
+})
+
+export {authUser, getUserProfile, registerUser}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
-import {authUser} from '../controllers/userControllers.js'
+import {authUser, getUserProfile, registerUser} from '../controllers/userControllers.js'
+import { protect } from "../middleware/authMiddleware.js";
 const router=express.Router()
 
 router.post('/login', authUser)
+router.route('/profile').get(protect, getUserProfile)
+router.route('/').post(registerUser)
+
 export default router
 
